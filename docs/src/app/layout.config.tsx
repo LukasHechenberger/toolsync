f25ec25171d8ae23bc767d054ca4da315003d283
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-06-07 12:18:14 +0200

    style: Update layout settings

--- docs/src/app/layout.config.tsx
+++ docs/src/app/layout.config.tsx
@@ -14,10 +14,14 @@ export const baseOptions: BaseLayoutProps = {
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" aria-label="Logo">
           <circle cx={12} cy={12} r={12} fill="currentColor" />
         </svg>
-        My App
+        Toolsync
       </>
     ),
   },
+  themeSwitch: {
+    mode: 'light-dark-system',
+  },
+
   // see https://fumadocs.dev/docs/ui/navigation/links
   links: [],
 };
--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,11 +1,19 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata = {
+  title: {
+    template: '%s | Toolsync',
+    default: 'Toolsync',
+  },
+} satisfies Metadata;
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
