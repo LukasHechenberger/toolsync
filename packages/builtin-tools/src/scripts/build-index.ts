730a381ee7918cce0fb6ff8edcc2a00e0f47941d
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-06-10 15:56:35 +0200

    fix: Update builtin readme during build

--- packages/builtin-tools/src/scripts/build-index.ts
+++ packages/builtin-tools/src/scripts/build-index.ts
@@ -1,4 +1,5 @@
 import { writeFile } from 'fs/promises';
+import { MarkdownTemplate, Template } from '@toolsync/template';
 import { name, exports } from '../../package.json';
 import { join } from 'path';
 
@@ -21,3 +22,18 @@ const tools = (
   .sort((a, b) => a.name.localeCompare(b.name));
 
 await writeFile('./out/tools.json', JSON.stringify(tools, null, 2) + '\n', 'utf-8');
+
+const readme = await MarkdownTemplate.load('./README.md', {
+  notice: `Generated during build. Do not edit manually.`,
+});
+
+readme.update({
+  section: 'tools',
+  insert: 'bottom',
+  content: `## Tools
+
+${tools.map((tool) => `- ${[`**${tool.name}**`, ...(tool.description ? [tool.description] : [])].join(' - ')}`).join('\n')}
+`,
+});
+
+await readme.save();
