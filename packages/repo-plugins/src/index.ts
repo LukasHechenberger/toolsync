548b13f509d92c5fae756f11cd27b6a6e00d0e71
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-06-10 22:41:40 +0200

    docs: Add todo

--- packages/repo-plugins/src/index.ts
+++ packages/repo-plugins/src/index.ts
@@ -21,6 +21,7 @@ const repoPlugin = definePlugin({
             ci: {
               jobs: {
                 build: {
+                  // TODO: Move to turborepo toolsync plugin
                   env: {
                     TURBO_TEAM: '${{ vars.TURBO_TEAM }}',
                     TURBO_TOKEN: '${{ secrets.TURBO_TOKEN }}',
