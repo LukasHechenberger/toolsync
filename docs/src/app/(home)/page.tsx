e5658636ba231ca68a6c84c5bb9e4c7019ed25a3
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-06-10 09:29:56 +0200

    perf: Pre-render code blocks

--- docs/src/app/(home)/page.tsx
+++ docs/src/app/(home)/page.tsx
@@ -1,27 +1,41 @@
 import { description } from '../../../../package.json';
 import { Card, Cards } from 'fumadocs-ui/components/card';
-import { DynamicCodeBlock } from 'fumadocs-ui/components/dynamic-codeblock';
+import { compileMDX } from '@fumadocs/mdx-remote';
+import { getMDXComponents } from '@/mdx-components';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const { body: InstallCommand } = await compileMDX({
+    source: `\`\`\`shell title="TL;DR"
+pnpm create @toolsync
+\`\`\``,
+  });
+
+  const { body: ConfigFile } = await compileMDX({
+    source: `\`\`\`jsonc title="toolsync.json"
+${JSON.stringify(
+  {
+    '@toolsync/builtin/package-readme': {},
+  },
+  null,
+  2,
+)}
+\`\`\``,
+  });
+
   return (
-    <main className="flex flex-1 flex-col container mx-auto justify-around text-center space-y-30">
-      <div>
+    <main className="flex flex-1 flex-col container mx-auto justify-around space-y-30">
+      <div className="min-h-[300px] flex flex-col items-center justify-center space-y-4">
         <h1 className="mb-4 text-2xl font-bold">Toolsync</h1>
         <p className="text-fd-muted-foreground">{description}</p>
+
+        <div className="text-left">
+          <InstallCommand components={getMDXComponents()} />
+        </div>
       </div>
 
       <div className="flex gap-4">
         <div className="flex-1 text-left">
-          <DynamicCodeBlock
-            lang="jsonc"
-            code={`// toolsync.json\n\n${JSON.stringify(
-              {
-                '@toolsync/builtin/package-readme': {},
-              },
-              null,
-              2,
-            )}`}
-          />
+          <ConfigFile components={getMDXComponents()} />
         </div>
         <div className="flex-1 prose text-left flex items-center">
           <ul>
@@ -31,7 +45,7 @@ export default function HomePage() {
       </div>
 
       <div>
-        <p className="mb-4 text-small">Still interested? Let&apos;s get you set up! 🎉</p>
+        <p className="mb-4 text-sm text-center">Still interested? Let&apos;s get you set up! 🎉</p>
         <Cards>
           <Card
             title="Get started"
--- packages/builtin-tools/README.md
+++ packages/builtin-tools/README.md
@@ -14,7 +14,7 @@ Default toolsync available
 
 ## Tools
 
-- **@toolsync/builtin/ignore-sync** - Integrates the 'ignore-sync' tool to manage .gitignore, .prettierignore, etc. files across multiple directories.
+- **@toolsync/builtin/ignore-sync** - Integrates the 'ignore-sync' tool to manage .gitignore, .prettierignore, etc.
 - **@toolsync/builtin/vscode** - Integrates with Visual Studio Code for settings and extensions management
 - **@toolsync/builtin/prettier** - Integrates with Prettier for code formatting
 - **@toolsync/builtin/github-actions** - Integrates with GitHub Actions for CI workflows
