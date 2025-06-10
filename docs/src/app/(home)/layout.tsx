58f15ce2272aa18b054819acb165a0963c3fc6a8
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-06-10 23:47:02 +0200

    docs: Move debugging to hosted docs (#20)

--- docs/src/app/(home)/layout.tsx
+++ docs/src/app/(home)/layout.tsx
@@ -3,5 +3,15 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/app/layout.config';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return (
+    <HomeLayout
+      {...baseOptions}
+      links={[
+        // Link to getting started page
+        { text: 'Docs', url: '/docs' },
+      ]}
+    >
+      {children}
+    </HomeLayout>
+  );
 }
