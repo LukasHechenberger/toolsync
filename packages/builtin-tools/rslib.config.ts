936011e4420f7a20f89b9519e98eb4692409fad6
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-11-10 11:11:40 +0100

    chore: Migrate to rslib

---
+++ packages/builtin-tools/rslib.config.ts
@@ -0,0 +1,83 @@
+import { writeFile } from 'fs/promises';
+import { MarkdownTemplate } from '@toolsync/template';
+import { name, exports, homepage } from './package.json';
+import { join } from 'path';
+import { markdownTable } from 'markdown-table';
+import { defineConfig, Rspack } from '@rslib/core';
+
+const buildIndexPlugin = {
+  name: 'build-index',
+  apply(compiler) {
+    const logger = compiler.getInfrastructureLogger('build-index');
+
+    compiler.hooks.done.tapPromise('build-index', async () => {
+      logger.info('Building index...');
+      const tools = (
+        await Promise.all(
+          Object.keys(exports)
+            .filter((e) => e !== '.' && !e.endsWith('.json'))
+            .map(async (path) => {
+              return {
+                path,
+                ...((await import(join(name, path))).default as {
+                  name: string;
+                  description: string;
+                }),
+              };
+            }),
+        )
+      )
+        .map((plugin) => ({
+          slug: plugin.name.replace('@toolsync/builtin/', ''),
+          ...plugin,
+        }))
+        .sort((a, b) => a.name.localeCompare(b.name));
+
+      await writeFile('./out/tools.json', JSON.stringify(tools, null, 2) + '\n', 'utf-8');
+
+      await MarkdownTemplate.update('./README.md', {
+        notice: `Generated during build. Do not edit manually.`,
+        section: 'tools',
+        content: `## Tools
+
+${markdownTable([
+  ['Name', 'Description', 'Links'],
+  ...tools.map((tool) => [
+    `[${tool.name}](./src/${tool.slug})`,
+    tool.description || '',
+    `[Documentation](${new URL(`./docs/builtin/${tool.slug}`, homepage)})`,
+  ]),
+])}
+`,
+      });
+    });
+  },
+} satisfies Rspack.RspackPluginInstance;
+
+export default defineConfig({
+  source: {
+    entry: {
+      index: 'src/index.ts',
+
+      ...Object.fromEntries(
+        Object.keys(exports)
+          .filter((key) => key !== '.' && !key.endsWith('.json'))
+          .map((key) => [`${key.slice(2)}/index`, `src/${key.slice(2)}/index.ts`]),
+      ),
+    },
+  },
+  lib: [
+    {
+      format: 'esm',
+      dts: true,
+      output: {
+        distPath: 'out',
+      },
+    },
+  ],
+  tools: {
+    rspack: {
+      plugins: [buildIndexPlugin],
+    },
+  },
+});
--- packages/builtin-tools/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/*.ts', 'src/*/index.ts', 'src/scripts/*.ts'],
-  outDir: 'out',
-  format: 'esm',
-  dts: true,
-  onSuccess: 'node ./out/scripts/build-index.js',
-});
