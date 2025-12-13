892c5833ac8d60d82710e9d1ea9f3002f147ce2b
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-12-13 01:09:34 +0100

    style: More debug logs

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -1,6 +1,11 @@
 #! /usr/bin/env node
 
+import { logger } from '@toolsync/logger';
 import program from '.';
 import { name, version } from '../package.json';
+
+const log = logger.child('bin');
+
+log.debug(`Starting ${name} v${version}`);
 
 program.name(name).version(version).parse();
